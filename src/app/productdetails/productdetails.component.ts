6ba8c1925633c557542a7f702242a7c1f4a3cc0e
jms-1005 <96402058+jms-1005@users.noreply.github.com>
2023-08-03 16:40:44 -0700

    added activated route

--- src/app/productdetails/productdetails.component.ts
+++ src/app/productdetails/productdetails.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ns-productdetails',
   templateUrl: './productdetails.component.html',
 })
-export class ProductdetailsComponent {
+export class ProductdetailsComponent implements OnInit {
+  //we check for the product id in the URL
+  //take the id and pass the id to the Strapi API
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    let productID = this.route.snapshot.paramMap.get('id');
+    console.log('product id', productID);
+  }
 }
