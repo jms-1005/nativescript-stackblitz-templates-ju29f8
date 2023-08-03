d697eb568ff32fea4f7928d6dd6e94d5921348d6
jms-1005 <96402058+jms-1005@users.noreply.github.com>
2023-08-03 16:59:36 -0700

    added strapi product id api

--- src/app/productdetails/productdetails.component.ts
+++ src/app/productdetails/productdetails.component.ts
@@ -1,5 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+interface Product {
+  id: number;
+  attributes: {
+    ProductName: string;
+    Price: string;
+    ProductDescription: string;
+  };
+}
 
 @Component({
   selector: 'ns-productdetails',
@@ -8,10 +18,21 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductdetailsComponent implements OnInit {
   //we check for the product id in the URL
   //take the id and pass the id to the Strapi API
-  constructor(private route: ActivatedRoute) {}
+  product: Product;
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit() {
     let productID = this.route.snapshot.paramMap.get('id');
     console.log('product id', productID);
+    this.http
+      .get<any>(
+        'https://starfish-app-ux8rd.ondigitalocean.app/api/products/' +
+          productID +
+          '?populate=*'
+      )
+      .subscribe((res) => {
+        console.log('RESPONSE:', res);
+        this.product = res.data;
+      });
   }
 }
